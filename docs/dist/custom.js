366ae8aac982774da68d873f9987be926deffb7b
Nico Kahlert <nico.kahlert@outlook.de>
2021-06-15 12:40:19 +0200

    MOD Quellen

--- docs/dist/custom.js
+++ docs/dist/custom.js
@@ -1,7 +1,6 @@
 // More info about initialization & config:
 // - https://revealjs.com/initialization/
 // - https://revealjs.com/config/
-mermaid.initialize({startOnLoad:true});
 Reveal.initialize({
   hash: true,
   controlsTutorial: true,
@@ -13,60 +12,12 @@ Reveal.initialize({
   touch: true,
   transition: "slide",
   autoAnimate: true,
+  dependencies: [
+    { src: 'plugin/mermaid/mermaid.js' }
+  ],
   // Learn about plugins: https://revealjs.com/plugins/
-  plugins: [RevealMarkdown, RevealHighlight, RevealNotes, RevealChart, ],
+  plugins: [RevealMarkdown, RevealHighlight, RevealNotes, ],
   // Chart Config
-  chart: {
-    defaults: {
-      global: {
-        title: { fontColor: "#000" },
-        legend: {
-          labels: { fontColor: "#000" },
-        },
-      },
-      scale: {
-        scaleLabel: { fontColor: "#000" },
-        gridLines: { color: "#000", zeroLineColor: "#000" },
-        ticks: { fontColor: "#000" },
-      },
-    },
-    line: {
-      borderColor: [
-        "rgba(20,220,220,.8)",
-        "rgba(220,120,120,.8)",
-        "rgba(20,120,220,.8)",
-      ],
-      borderDash: [
-        [5, 10],
-        [0, 0],
-      ],
-    },
-    bar: {
-      backgroundColor: [
-        "rgba(20,220,220,.8)",
-        "rgba(220,120,120,.8)",
-        "rgba(20,120,220,.8)",
-      ],
-    },
-    pie: {
-      backgroundColor: [
-        [
-          "rgba(0,0,0,.8)",
-          "rgba(220,20,20,.8)",
-          "rgba(20,220,20,.8)",
-          "rgba(220,220,20,.8)",
-          "rgba(20,20,220,.8)",
-        ],
-      ],
-    },
-    radar: {
-      borderColor: [
-        "rgba(20,220,220,.8)",
-        "rgba(220,120,120,.8)",
-        "rgba(20,120,220,.8)",
-      ],
-    },
-  },
 }).then(() => {
   // reveal.js is ready
   // Create QR Code dynamically
